36e82ee4c1b7a0e625fa8c82843c6b0ca677d7c0
adhtri001 <adheeshtrivedi@gmail.com>
2026-04-12 19:49:19 +0530

    Fixed file path in example

--- examples/schedule.py
+++ examples/schedule.py
@@ -1,4 +1,4 @@
 """
 This file will use the `unisched` library to schedule the
-anonymized registration data in `anonymous-registration-data.csv`
+anonymized registration data in `data/anonymous-registration-data.csv`
 """
